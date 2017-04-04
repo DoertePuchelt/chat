f0c6d9bad43ac532f151c3dec2b3f98836b0dcf5
Tobias Lampprecht <tobias.lampprecht@gmail.com>
2017-04-04 14:38:43 +0200

    added username

--- index.js
+++ index.js
@@ -7,24 +7,54 @@ var app = require("express")();
 
 var http = require("http").Server(app);
 
-var io= require("socket.io")(http);
+var io = require("socket.io")(http);
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
 
 
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname +"/index.html");
 });
-io.on("connection", function(socket){
-	io.emit('connected', "user connected");
-	console.log("user connected");
+
+var user = "";
+
+app.post("/", function(req, res){
+	res.sendFile(__dirname +"/chat.html");
+	user = req.body.username;
+});
+
+var userList = [];
+
+function timestamp(){
+	var date = new Date().toLocaleDateString();
+	var time = new Date().toLocaleTimeString();
+	return "[" + date + " " + time + "] ";
+}
+
+io.on("connection", function(client){
+	client.join("chatroom");
+	client.username = user;
 	
-	socket.on("chat message", function(msg){
-		io.emit("chat message", msg)
+	userList.push({username:client.username, userID:client.id});
+	
+	io.emit('connected', ""+timestamp()+"'"+client.username+"' connected");
+//	console.log("user connected");
+//	console.log(client.id);
+	
+//	var clients = io.sockets.adapter.rooms['chatroom'];
+//	console.log(clients);
+//	var c = io.sockets.clients();
+//	console.log(c);
+	client.on("chat message", function(msg){
+		io.emit("chat message", {timestamp:timestamp(), username:client.username, message:msg})
 	});
 	
-	socket.on('disconnect', function(){
-		io.emit("disconnect", "user disconnected");
-		console.log('user disconnected');
+	client.on('disconnect', function(){
+		io.emit("disconnect", ""+timestamp()+"'"+client.username+"' disconnected");
+//		console.log('user disconnected');
 	});
 });
 
