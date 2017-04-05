a3204d2ef0bd32ff3a59fd200f7bce5027393d54
Tobias Lampprecht <tobias.lampprecht@gmail.com>
2017-04-05 21:36:01 +0200

    bootstrap login screen; documented code

--- server.js
+++ server.js
@@ -14,41 +14,50 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-
+// Root File for new connections
 app.get("/", function(req, res){
 	res.sendFile(__dirname +"/index.html");
 });
 
 var user = "";
 
+// receive the username with a POST-Request from index.html 
+// and forward the client to chat.html
 app.post("/", function(req, res){
 	res.sendFile(__dirname +"/chat.html");
 	user = req.body.username.trim();
 });
 
-
+// List of connected Clients
 var userList = {};
 
+// receive the current timestamp
 function timestamp(){
 	var date = new Date().toLocaleDateString();
 	var time = new Date().toLocaleTimeString();
 	return "[" + date + " " + time + "] ";
 }
 
+// handle new connections
 io.on("connection", function(client){
 	client.join("chatroom");
 	client.username = user;
 	
+	// add the client to userList
 	userList[client.username] = client;
 	
+	// send the 'connected' event to all clients, with the name of the new client
 	io.emit('connected', ""+timestamp()+"'"+client.username+"' connected");
 
-	
+	// handle 'userlist' event to send the list of all clients
+	// to the client that requested
 	client.on("userlist", function(){
 		var onlineUsers = Object.getOwnPropertyNames(userList);
 		userList[client.username].emit('userlist', onlineUsers);
 	});
 	
+	// handle 'private message' event
+	// send the message only to the sender and receiver
 	client.on('private message', function(text){
 		text = text.substr(1, text.length);
 		var userInput = text.split(" ");
@@ -61,16 +70,21 @@ io.on("connection", function(client){
 		userList[client.username].emit("chat message", {timestamp:timestamp(), username:privateUserInfoSender, message:privateMessage});
 	});
 	
+	// handle 'chat message' event
+	// send message to all connected clients
 	client.on("chat message", function(msg){
 		io.emit("chat message", {timestamp:timestamp(), username:client.username, message:msg});
 	});
 	
+	// handle 'disconnect' event
+	// send to all clients which user disconnected
 	client.on('disconnect', function(){
 		delete userList[client.username];
 		io.emit("disconnect", ""+timestamp()+"'"+client.username+"' disconnected");
 	});
 });
 
+// start server on port 8888
 http.listen(8888, function(){
 	console.log("listening on*:8888");
 });
